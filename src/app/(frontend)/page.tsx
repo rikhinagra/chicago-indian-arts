415830a3053d216db577abc8dbbc081d79c4372a
Chandan-devs-tech <chandanguptabsb@gmail.com>
2026-02-12 20:13:18 +0530

    Remove Newsletter, Stats and CTA sections from homepage

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -6,9 +6,6 @@ import EventsTimeline from "@/components/home/EventsTimeline";
 import HighlightsGallery from "@/components/home/HighlightsGallery";
 import FeaturedSpeakers from "@/components/home/FeaturedSpeakers";
 import SponsorsSection from "@/components/home/SponsorsSection";
-import NewsletterSignup from "@/components/home/NewsletterSignup";
-import StatsCounters from "@/components/home/StatsCounters";
-import CTASection from "@/components/home/CTASection";
 
 export default function HomePage() {
   return (
@@ -21,9 +18,6 @@ export default function HomePage() {
       <HighlightsGallery />
       <FeaturedSpeakers />
       <SponsorsSection />
-      <NewsletterSignup />
-      <StatsCounters />
-      <CTASection />
     </>
   );
 }
