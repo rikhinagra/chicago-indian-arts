85eddcc51722cbdcb7d19dcf31c50aa2ae1e421e
Chandan-devs-tech <chandanguptabsb@gmail.com>
2026-02-12 20:15:20 +0530

    Restore Stats and CTA sections on homepage

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -6,6 +6,8 @@ import EventsTimeline from "@/components/home/EventsTimeline";
 import HighlightsGallery from "@/components/home/HighlightsGallery";
 import FeaturedSpeakers from "@/components/home/FeaturedSpeakers";
 import SponsorsSection from "@/components/home/SponsorsSection";
+import StatsCounters from "@/components/home/StatsCounters";
+import CTASection from "@/components/home/CTASection";
 
 export default function HomePage() {
   return (
@@ -18,6 +20,8 @@ export default function HomePage() {
       <HighlightsGallery />
       <FeaturedSpeakers />
       <SponsorsSection />
+      <StatsCounters />
+      <CTASection />
     </>
   );
 }
